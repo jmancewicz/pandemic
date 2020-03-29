22f00eb034d823cf7f41725042094f142d69ce5e
Joe Mancewicz <jmancewicz@nvidia.com>
2020-03-29 12:07:52 -0700

    PEP8 compliance and restructure

--- pandemic.py
+++ pandemic.py
@@ -1,84 +1,100 @@
 #!/usr/bin/env python
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-PercentSick = 0.5
-PercentHospitalized = 0.2
-iMinHospitalStay = 12
-MaxHospital = 20.0
 
-L = 10000.0 * PercentSick
-Tau = 28.0
-K = 1.0 / Tau
+class Data:
+    threshold = 10
+    max_hospital = 20.0
+    offset = 365
+    percent_sick = 0.5
+    percent_hospitalized = 0.2
+    min_hospital_stay = 12
 
-threshold = 10
+    def __init__(self):
+        self.shelter_days = None
+        self.days = np.arange(-Data.offset, Data.offset, 1)
+        self.day1 = None
+        self.total_sick = None
+        self.people_in_hospital = np.zeros(self.days.size)
+        self.sick_per_day = None
 
-offset = 365
+    def compute(self):
 
-days = np.arange(-offset,offset,1)
+        sick = 10000.0 * Data.percent_sick
+        tau = 28.0
 
-e = np.exp(-K*days)
-ope = 1.0 + e
+        e = np.exp(-self.days / tau)
+        ope = 1.0 + e
 
-TotalSick = L / ope
-SickPerDay = L * K * e / ope**2
+        self.total_sick = sick / ope
+        self.sick_per_day = sick / tau * e / ope ** 2
 
-PeopleInHospital = np.zeros(offset*2)
+        for i in range(len(self.days)):
+            people_in = self.sick_per_day[i] * Data.percent_hospitalized
+            people_out = 0
 
-i = 0
-for iDay in days:
-    PeopleIn = SickPerDay[i] * PercentHospitalized
-    PeopleOut = 0
-    
-    if (i > iMinHospitalStay):
-        PeopleOut = SickPerDay[i-iMinHospitalStay] * PercentHospitalized
+            if i > Data.min_hospital_stay:
+                people_out = self.sick_per_day[i - Data.min_hospital_stay] * \
+                             Data.percent_hospitalized
 
-    PeopleInHospital[i] = PeopleInHospital[i-1] + PeopleIn - PeopleOut
-    i=i+1
+            self.people_in_hospital[i] = self.people_in_hospital[i - 1] + people_in - people_out
 
-l = TotalSick < threshold 
+        less_than = self.total_sick < Data.threshold
 
-Day1 = np.max(days[l])
+        self.day1 = np.max(self.days[less_than])
 
-ShelterDays = -2*Day1
+        self.shelter_days = -2 * self.day1
 
-x1 = [Day1 + offset,Day1 + offset]
-x2 = [-Day1 + offset, -Day1 + offset]
+    def plot(self):
+        title = 'Days to Shelter = ' + str(self.shelter_days) + ' (Threshold = ' + str(
+            Data.threshold * 1000) + ' people)'
 
-title = 'Days to Shelter = '+str(ShelterDays)+' (Threshold = '+str(threshold*1000)+' people)'
+        fig = plt.figure(figsize=(6, 10))
 
-fig = plt.figure(figsize=(6,10))
+        shift = Data.offset + self.day1
 
-shift = offset+Day1
+        ax = fig.add_subplot(311)
+        ax.plot(self.days + Data.offset - shift,
+                self.total_sick)
+        ax.set(xlabel='time (days)',
+               ylabel='Number of Sick (1,000 people)',
+               title=title,
+               xlim=[0, self.shelter_days])
+        ax.grid()
 
-ax = fig.add_subplot(311)
-ax.plot(days+offset-shift,TotalSick)
-y = [0,L]
-#ax.plot(x1-shift,y,'b--')
-#ax.plot(x2-shift,y,'b--')
-ax.set(xlabel='time (days)',
-       ylabel='Number of Sick (1,000 people)',
-       title=title, xlim=[0,ShelterDays])
-ax.grid()
+        ax2 = fig.add_subplot(312)
+        ax2.plot(self.days + Data.offset - shift,
+                 self.sick_per_day)
+        ax2.set(xlabel='time (days)',
+                ylabel='Sick Per Day (1,000 people)',
+                xlim=[0, self.shelter_days])
+        ax2.grid()
 
-ax2 = fig.add_subplot(312)
-ax2.plot(days+offset-shift,SickPerDay)
-y = [0,np.max(SickPerDay)]
-#ax2.plot(x1-shift,y,'b--')
-#ax2.plot(x2-shift,y,'b--')
-ax2.set(xlabel='time (days)', ylabel='Sick Per Day (1,000 people)',xlim=[0,ShelterDays])
-ax2.grid()
+        ax3 = fig.add_subplot(313)
+        ax3.plot(self.days + Data.offset - shift,
+                 self.people_in_hospital,
+                 label='People')
+        ax3.plot(self.days + Data.offset - shift,
+                 np.zeros(len(self.days)) + Data.max_hospital,
+                 'b--',
+                 label='Hosp. Cap.')
+        ax3.plot(self.days + Data.offset - shift,
+                 np.zeros(len(self.days)) + 2 * Data.max_hospital,
+                 'r--',
+                 label='2x Hosp. Cap.')
+        ax3.set(xlabel='time (days)',
+                ylabel='People In Hosp. (1,000 people)',
+                xlim=[0, self.shelter_days])
+        ax3.legend()
+        ax3.grid()
 
-ax3 = fig.add_subplot(313)
-ax3.plot(days+offset-shift,PeopleInHospital, label = 'People')
-ax3.plot(days+offset-shift,PeopleInHospital*0.0 + MaxHospital, 'b--', label = 'Hosp. Cap.')
-ax3.plot(days+offset-shift,PeopleInHospital*0.0 + 2*MaxHospital, 'r--', label = '2x Hosp. Cap.')
-y = [0,np.max(PeopleInHospital)]
-#ax3.plot(x1-shift,y,'b--')
-#ax3.plot(x2-shift,y,'b--')
-ax3.set(xlabel='time (days)', ylabel='People In Hosp. (1,000 people)',xlim=[0,ShelterDays])
-ax3.legend()
-ax3.grid()
+        fig.savefig("test.png")
+        plt.show()
 
-fig.savefig("test.png")
+
+if __name__ == '__main__':
+    data = Data()
+    data.compute()
+    data.plot()
